7a96e3c520d393502cedb5f96d438151392fea94
TechKeyMonk <development.techkeymonk@gmail.com>
2026-09-05 17:10:14 +0530

    fix: disabled Vercel API caching to show fresh edits globally

--- api/db.js
+++ api/db.js
@@ -6,6 +6,9 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   if (req.method === 'OPTIONS') {
     res.writeHead(200).end();
     return;
