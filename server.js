1e2a211748146ed318b7197f7ce65c71369513b5
TechKeyMonk <development.techkeymonk@gmail.com>
2026-09-05 18:20:56 +0530

    fix: added local server cache-control headers to fix UI sync issues

--- server.js
+++ server.js
@@ -256,6 +256,9 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
 
   if (req.method === 'OPTIONS') {
     res.writeHead(204);
